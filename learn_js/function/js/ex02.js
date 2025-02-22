427e55ba19e23af6f9793e090011504ac9b18b35
Duy Riba <tranduy.riba@gmail.com>
2025-02-22 17:03:23 +0700

    Bai 8

--- learn_js/function/js/ex02.js
+++
@@ -1,9 +0,0 @@
-prompt("Nhập tuổi của bạn")
-
-alert("Alo")
-
-// setTimout sẽ chỉ chạy 1 lần sau x giây
-setTimeout(() => {}, 1000)
-
-// Hàm setInterval sau 2s sẽ chạy tiếp các lần tioeeps theo
-setInterval(() => {}, 2000)
--- learn_js/operator/index.js
+++
@@ -1,9 +0,0 @@
-let b;
-
-if (b = underfine) {
-    console.log("underfine")
-} else {
-    console.log("Hello")
-}
-
-console.log(b)
